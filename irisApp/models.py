17eaf462a73d753224cea93bf14b1691c4e05922
mhd <m.almouhtaseb@gmail.com>
2024-04-05 05:05:25 +0300

    irisApp & csrf

---
+++ irisApp/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from users.models import *
+from datetime import date
+
+# Create your models here.
+
+class Panic_Disorder(models.Model):
+
+    Person_Id = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='Person_Id')
+    Doctor_Id = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True, blank=True, related_name='Doctor_Id') 
+    Positive_Negative=models.BooleanField(null=True, blank=True)
+    Family_History=models.IntegerField(null=True, blank=True)
+    Personal_History=models.IntegerField(null=True, blank=True)
+    Current_Stressors=models.IntegerField(null=True, blank=True)
+    Symptoms=models.IntegerField(null=True, blank=True)
+    Severity=models.IntegerField(null=True, blank=True)
+    Impact_on_Life=models.IntegerField(null=True, blank=True)
+    Demographics=models.IntegerField(null=True, blank=True)
+    Medical_History=models.IntegerField(null=True, blank=True)
+    Psychiatric_History=models.IntegerField(null=True, blank=True)
+    Substance_Use=models.IntegerField(null=True, blank=True)
+    Coping_Mechanisms=models.IntegerField(null=True, blank=True)
+    Social_Support=models.IntegerField(null=True, blank=True)
+    Lifestyle_Factors=models.IntegerField(null=True, blank=True)
+
+
+    def __str__(self):
+        return  str(self.Person_Id)
+    
+    class Meta:
+        verbose_name = 'Panic_Disorder'
+        ordering = ['Person_Id']
--- mainproject/settings.py
+++ mainproject/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-g(apslr*qaqw)%$!4o%p2a83&5kbb60+wlc0!bvp4c^mphps+p"
 
+CSRF_TRUSTED_ORIGINS = ['https://selene-m-h.up.railway.app']
+
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost','.up.railway.app','*']
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "users",
     "to_do_list",
+    "irisApp",
 ]
 
 MIDDLEWARE = [
