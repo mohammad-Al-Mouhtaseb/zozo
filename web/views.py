7939a51f6c99cb6c9af7c8845583463c9215a028
mhd <m.almouhtaseb@gmail.com>
2024-04-06 11:22:57 +0300

    first commit

--- web/views.py
+++ web/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def google_search(request):
+    return render(request,'googlef07a8177d651b4db.html')
