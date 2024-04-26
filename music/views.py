89bed02a2d9bdf546780ed189540c76dbfaad9af
mhd <m.almouhtaseb@gmail.com>
2024-04-26 22:10:09 +0300

    remove edit music for nour

--- music/views.py
+++ music/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, FileResponse, HttpResponseRedirect
 import requests
 
-from transformers import pipeline
-import scipy
+# from transformers import pipeline
+# import scipy
 
-synthesiser = pipeline("text-to-audio", "facebook/musicgen-small")
+# synthesiser = pipeline("text-to-audio", "facebook/musicgen-small")
 
 # Create your views here.
 def test(request,name):
@@ -19,7 +19,7 @@ def test(request,name):
     print(response.json()[0]['url'])
     return HttpResponseRedirect(response.json()[0]['url'])
 
-def create(request,text):
-    music = synthesiser(text, forward_params={"do_sample": True})
-    scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])
-    return HttpResponse(music["audio"], mimetype="audio/mpeg") 
+# def create(request,text):
+    # music = synthesiser(text, forward_params={"do_sample": True})
+    # scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])
+    # return HttpResponse(music["audio"], mimetype="audio/mpeg") 
