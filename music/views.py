061a0c394aab2d7aaad960cec68911ee2dabe628
mhd <m.almouhtaseb@gmail.com>
2024-04-26 21:11:05 +0300

    edit 1

--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse, FileResponse, HttpResponseRedirect, http
+from django.http import JsonResponse, HttpResponse, FileResponse, HttpResponseRedirect
 import requests
 
 from transformers import pipeline
