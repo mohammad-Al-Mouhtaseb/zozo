51f1ea561be30e19fc211033e020b39cf80362a2
mhd <m.almouhtaseb@gmail.com>
2024-04-30 20:39:37 +0300

    device & sampling_rate

--- music/views.py
+++ music/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, FileResponse, HttpResponseRedirect
 import requests
 
-from transformers import AutoProcessor
+from transformers import AutoProcessor, MusicgenForConditionalGeneration
 import scipy
 
+model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+
 processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+
+import torch
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+model.to(device)
+sampling_rate = model.config.audio_encoder.sampling_rate
 
 
 # Create your views here.
