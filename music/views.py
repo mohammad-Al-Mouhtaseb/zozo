4e19dc2ed3a05c3181c140fa6020381489f38c8c
mhd <m.almouhtaseb@gmail.com>
2024-05-01 10:47:34 +0300

    1/5

--- music/views.py
+++ music/views.py
@@ -9,9 +9,9 @@ import scipy
 # processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
 # import torch
 # device = "cuda:0" if torch.cuda.is_available() else "cpu"
-device="cpu"
-model.to(device)
-sampling_rate = model.config.audio_encoder.sampling_rate
+# device="cpu"
+# model.to(device)
+# sampling_rate = model.config.audio_encoder.sampling_rate
 
 
 # Create your views here.
@@ -27,12 +27,13 @@ def test(request,name):
     return HttpResponseRedirect(response.json()[0]['url'])
 
 def create(request,text):
-    inputs = processor(
-        text=[text],
-        padding=True,
-        return_tensors="pt",
-    )
-    audio_values = model.generate(**inputs.to(device), do_sample=True, guidance_scale=3, max_new_tokens=256)
-    scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values)
+    # inputs = processor(
+    #     text=[text],
+    #     padding=True,
+    #     return_tensors="pt",
+    # )
+    # audio_values = model.generate(**inputs.to(device), do_sample=True, guidance_scale=3, max_new_tokens=256)
+    # scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values)
 
-    return HttpResponse(sampling_rate) 
+    # return HttpResponse(sampling_rate) 
+    pass
