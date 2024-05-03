7f1777cf87144699b6a380983d9d8611042d01a9
mhd <m.almouhtaseb@gmail.com>
2024-05-03 11:10:46 +0300

    experta

--- irisApp/urls.py
+++ irisApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('firstquiz',views.firstquiz,name='firstquiz'),
     path('Panic_Disorder',views.Panic,name='Panic_Disorder'),
     path('QPanic_Disorder',views.QPanic,name='QPanic_Disorder'),
     path('Panic_view',views.doctor_view,name='doctor_view'),
--- irisApp/views.py
+++ irisApp/views.py
@@ -2,9 +2,59 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, FileResponse
 from django.views.decorators.csrf import csrf_exempt
 import datetime ,json
+from experta import *
 from users.views import *
 from . models import *
 from joblib import load
+
+@csrf_exempt
+def firstquiz(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        person_resul=check_token(request)
+        if check_token(request):
+            a1=bool(data['a1'])#Stress
+            a2=bool(data['a2'])#Anxiety
+            a3=bool(data['a3'])#Depression
+            a4=bool(data['a4'])#Stress
+            a5=bool(data['a5'])#Depression
+            a6=bool(data['a6'])#Anxiety
+            a7=bool(data['a7'])#Depression
+            a8=bool(data['a8'])#Anxiety
+
+            class Robot(KnowledgeEngine):
+                
+                @Rule(NOT(Fact(Depression=W())))
+                def Depression(self):
+                    self.declare(Fact(Depression=bool(a3 and a5)or(a5 and a7)or(a3 and a7)))
+
+                @Rule((Fact(Depression=W())) and (NOT(Fact(Anxiety=W()))))
+                def Anxiety(self):
+                    self.declare(Fact(Anxiety=bool((a2 and a6)or(a6 and a8)or(a2 and a8))))
+
+                @Rule((Fact(Anxiety=W())) and (NOT(Fact(Stress=W()))))
+                def Stress(self):
+                    self.declare(Fact(Stress=bool(a1 or a4)))
+                    
+            engine = Robot()
+            engine.reset()
+            engine.run()
+            facts=list(engine.facts.items())
+            d=str(facts[1])
+            a=str(facts[2])
+            s=str(facts[3])
+            d=d[9:len(d)-2]
+            a=a[9:len(a)-2]
+            s=s[9:len(s)-2]
+            d=d.split('=')
+            a=a.split('=')
+            s=s.split('=')
+            
+            return JsonResponse({d[0]:d[1],a[0]:a[1],s[0]:s[1]}, status=200)
+        else:
+            return exp_logout(request)
+    return JsonResponse({'state':'error request method'}, status=201)
+
 
 all_q=['Family_History','Personal_History','Current_Stressors','Symptoms','Severity','Impact_on_Life','Demographics','Medical_History','Psychiatric_History','Substance_Use','Coping_Mechanisms','Social_Support','Lifestyle_Factors']
 @csrf_exempt
