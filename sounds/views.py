2e7e105164e208082d3cf1064cb0ea8043b32080
mhd <m.almouhtaseb@gmail.com>
2024-06-02 23:11:56 +0300

    print

--- sounds/views.py
+++ sounds/views.py
@@ -44,7 +44,9 @@ def gen_fun(request):
         audio_bytes = {
             "inputs": desc,
         }
+        print("111")
         response = requests.post(API_URL, headers=headers, json=audio_bytes, timeout=120)
+        print("222")
         music=Music.objects.create(doctor=doctor,patient=patient,music=ContentFile(response.content, name=type+'_'+desc+'.flac'),music_path=music_path,type=type)
         music.save()
     except Exception as e:
