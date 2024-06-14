5927eadc3f49ff3f1bdfdfb7ef6a523e50d42678
mhd <m.almouhtaseb@gmail.com>
2024-06-14 11:07:35 +0300

    urls

--- irisApp/urls.py
+++ irisApp/urls.py
@@ -6,16 +6,14 @@ urlpatterns = [
 
     path('panic_pisorder',views.Panic,name='Panic_Disorder'),
     path('dep_bi',views.Dep_Bi,name='Dep_Bi'),
+    path('p_dep',views.P_Dep,name='P_Dep'),
+    
+    path('q_panic_disorder',views.QPanic,name='QPanic_Disorder'),
+    path('q_dep_bi',views.QDep_Bi,name='QDep_Bi'),
+    path('q_p_dep',views.QP_Dep,name='QP_Dep'),
 
 
     path('select_doctor',views.select_doctor,name='select_doctor'),
     path('show_my_patient',views.patient_list_for_doctor,name='patient_list_for_doctor'),
     path('doctor_view_data',views.doctor_view,name='doctor_view'),
-
-
-    path('q_panic_disorder',views.QPanic,name='QPanic_Disorder'),
-    path('q_dep_bi',views.QDep_Bi,name='QDep_Bi'),
 ]
-#Depression = depression
-#Panic attack = anxiety
-#Stress = bipolar
