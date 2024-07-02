b9b2c284828596849a9dd3bf93c60ff0c2e00132
mhd <m.almouhtaseb@gmail.com>
2024-07-02 20:29:15 +0300

    ggg

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from . models import *
 
-class CustomUserAdmin(admin.ModelAdmin):
-    exclude = ('is_superuser','user_permissions','last_login','token','private_key')
-admin.site.register(User, CustomUserAdmin)
+class CustomUser(admin.ModelAdmin):
+    exclude = ('is_superuser','user_permissions','last_login','token','private_key','date_joined')
+admin.site.register(User, CustomUser)
 
+class CustomPatient(admin.ModelAdmin):
+    exclude = ('is_superuser','user_permissions','last_login','token','private_key','groups','type','is_staff','name','date_joined')
+admin.site.register(Patient, CustomPatient)
 
-admin.site.register(Patient, CustomUserAdmin)
-admin.site.register(Doctor, CustomUserAdmin)
+class CustomDoctor(admin.ModelAdmin):
+    exclude = ('is_superuser','user_permissions','last_login','token','private_key','groups','type','is_staff','num_of_rate','name','date_joined')
+admin.site.register(Doctor, CustomDoctor)
