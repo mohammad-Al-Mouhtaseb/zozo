d0d8d90f6d446c8c0f01647aed5df342f0fc509d
mhd <m.almouhtaseb@gmail.com>
2024-07-05 14:48:12 +0300

    web

--- web/views.py
+++ web/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-
-def google_search(request):
-    return render(request,'googlef07a8177d651b4db.html')
